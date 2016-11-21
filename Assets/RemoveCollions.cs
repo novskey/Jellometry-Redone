a4bcf63f5e2b58e2836e59994e8e9b09390d4524
Paris Larkins <paris.larkins@gmail.com>
2016-11-22 00:36:56 +1100

    Playing around with decals and projectiles causing paint splats

--- Assets/RemoveCollions.cs
+++ Assets/RemoveCollions.cs
@@ -15,7 +15,15 @@ public class RemoveCollions : MonoBehaviour {
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.name != "Player")
+        if (other.gameObject.tag == "Projectile")
+        {
+            float y = Random.rotation.y * 100;
+            float z = Random.rotation.z*100;
+
+            GameObject splat = (GameObject) Instantiate(GameObject.Find("Splat"), other.transform.position,Quaternion.Euler(90,y,z));
+
+            Destroy(other.gameObject);
+        }else if (other.gameObject.name != "Player")
         {
             Destroy(other.gameObject);
         }
