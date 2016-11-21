6ac2f32508b84134dd33fc16884b10904f9a04e0
parislarkins <paris.larkins@gmail.com>
2016-11-22 10:40:23 +1100

    Fixed a bug in the splat demo where the splat would be invisible until selected in the inspector

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,52 +1,55 @@
 ﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Assets.Scripts
 {
-
-    private float _health = 100;
-
-    private IWeapon _weapon;
-
-    // Use this for initialization
-    void Start ()
+    public class Player : MonoBehaviour
     {
-        _weapon = GameObject.Find("pistol").GetComponent<Pistol>();
-    }
-	
-    // Update is called once per frame
-    void Update () {
-        if (Input.GetButton("Fire1"))
+
+        private float _health = 100;
+
+        private IWeapon _weapon;
+
+        // Use this for initialization
+        void Start ()
         {
-            _weapon.Fire();
+            _weapon = GameObject.Find("pistol").GetComponent<Pistol>();
         }
-    }
 
-    void AddModifier(Mod modifier)
-    {
-        switch (modifier.Target)
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetButton("Fire1"))
+            {
+                _weapon.Fire();
+            }
+        }
+
+        void AddModifier(Mod modifier)
         {
-            case "hp":
-                Debug.Log(_health);
-                _health = (int)(_health * modifier.Modifier);
-                Debug.Log(_health);
-                break;
-            case "fr":
-                _weapon.FireDelay = (float) (_weapon.FireDelay*modifier.Modifier);
-                Debug.Log(_weapon.FireDelay);
-                break;
+            switch (modifier.Target)
+            {
+                case "hp":
+                    Debug.Log(_health);
+                    _health = (int)(_health * modifier.Modifier);
+                    Debug.Log(_health);
+                    break;
+                case "fr":
+                    _weapon.FireDelay = (float) (_weapon.FireDelay*modifier.Modifier);
+                    Debug.Log(_weapon.FireDelay);
+                    break;
+            }
         }
-    }
 
-    void RemoveModifier(Mod modifier)
-    {
-        switch (modifier.Target)
+        void RemoveModifier(Mod modifier)
         {
-            case "hp":
-                _health = (int)(_health / modifier.Modifier);
-                Debug.Log(_health);
-                break;
+            switch (modifier.Target)
+            {
+                case "hp":
+                    _health = (int)(_health / modifier.Modifier);
+                    Debug.Log(_health);
+                    break;
+            }
         }
+
+
     }
-
-
 }
