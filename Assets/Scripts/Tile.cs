bfa6995adaa3d166089a07f6158e638a231a1fc2
parislarkins <paris.larkins@gmail.com>
2016-11-28 17:55:29 +1100

    Added some initial base files for map gen

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Tile
+    {
+        private GameObject _object;
+        private List<JoinNode> _joinNodes;
+
+        public Tile(GameObject gameObject, List<JoinNode> nodes)
+        {
+            _object = gameObject;
+            _joinNodes = nodes;
+        }
+    }
+
+    public class JoinNode
+    {
+        private NodeDir dir;
+
+
+        public JoinNode(Transform transform)
+        {
+        }
+    }
+
+    public enum NodeDir
+    {
+        N,
+        S,
+        E,
+        W
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class TileManager : MonoBehaviour
+{
+    private List<GameObject> _tileObjects;
+
+    private List<Tile> _tiles;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _tileObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("Join Node"));
+
+	    PlaceTiles();
+	}
+
+    private void PlaceTiles()
+    {
+
+        foreach (GameObject tile in _tileObjects)
+        {
+            List<JoinNode> joinNodes = new List<JoinNode>();
+            for (int i = 0; i < 3; i++)
+            {
+                if (tile.transform.GetChild(i).transform.childCount != 0)
+                {
+                    joinNodes.Add(new JoinNode(tile.transform.GetChild(i));
+                }
+            }
+
+            _tiles.Add(new Tile(tile, joinNodes));
+        }
+    }
+
+	void Update () {
+    // Update is called once per frame
+
+	   
+	    
+	}
+}
